8f2796f9e28446858de2ff32b98a81285daaee48
Ryan Wallerius <ryanwallerius@yahoo.com>
2019-05-20 18:02:51 -0700

    Trying to add a new script

--- Ansible_Controller/hw7.sh
+++ Ansible_Controller/hw7.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-yum install -y ansible
+sudo yum install -y ansible
 
 ssh-keygen -t rsa
-ssh-copy-id-i ~/.ssh/id_rsa root@192.168.1.20
-ssh-copy-id-i ~/.ssh/id_rsa root@192.168.1.21
-ssh-copy-id-i ~/.ssh/id_rsa root@192.168.1.22
-ssh-copy-id-i ~/.ssh/id_rsa root@192.168.1.23
+ssh-copy-id -i ~/.ssh/id_rsa root@192.168.1.20
+ssh-copy-id -i ~/.ssh/id_rsa root@192.168.1.21
+ssh-copy-id -i ~/.ssh/id_rsa root@192.168.1.22
+ssh-copy-id -i ~/.ssh/id_rsa root@192.168.1.23
 
 ansible-playbook webserver.yaml -i ./hosts.ini
 curl 192.168.1.20
